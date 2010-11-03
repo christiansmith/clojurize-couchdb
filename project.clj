f0acee795e5c1dd42ec9861ea2144b6cbee9f272
Christian Smith <http://github.com/christiansmith>
2010-11-03 13:42:25 -0700

    use clojurize-protocol as a dependency

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Experimental Clojurize CouchDB Adapter"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [clojurize-protocol "1.0.0-SNAPSHOT"]
                  [clj-http "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
